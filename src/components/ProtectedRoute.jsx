875ac5e76665e5d7fd33441bacd2350c348f4fa0
Perevalov Anton <mormolad@yandex.ru>
2023-09-27 20:18:17 +0700

    test

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,12 +4,9 @@ import { Navigate } from 'react-router-dom';
 // этот компонент принимает другой компонент в качестве пропса
 // он также может взять неограниченное число пропсов и передать их новому компоненту
 const ProtectedRoute = ({ element: Component, ...props }) => {
-  console.log('logginIN в протектед', props.loggedIn);
   if (props.loggedIn) {
-    console.log('logginIN в протектед в условии true', props.loggedIn);
     return <Component {...props} />;
   } else {
-    console.log('logginIN в протектед в условии false', props.loggedIn);
     return <Navigate to="/sign-in" replace />;
   }
 };
